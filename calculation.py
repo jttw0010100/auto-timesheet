f09b3d2e0d7d0d8f1318d52605bc57574a4cc167
jttw0010100 <0010100@student.isf.edu.hk>
2022-07-19 16:23:14 +0800

    time diff calculation

--- calculation.py
+++ calculation.py
@@ -1,4 +1,8 @@
 import datetime as dt
+from lib2to3.pytree import convert
+from os import lseek
+from sqlite3 import Date
+from xmlrpc.client import DateTime
 
 class Req():
     #max weekly hours
@@ -8,17 +12,37 @@ class Req():
     #max date range
     maxdr = 59
 
-    def calcwh(start, lunchs, lunche, finish):
-        if finish-start+1>15:
-            return False
-        else:
-            return True
+    total_hours = 0
 
-class Datetime(Req):
+    def calcwh(start, lunchstart, lunchfinish, finish):
+        total_day_hours = convert(start,finish)
+        lunch_hours = convert(lunchstart,lunchfinish)
+        #work_hours = total_day_hours - lunch_hours
+        return Req.calctd(start,finish)
+        
+    def calctd(t1, t2):
+        hours = 0
+        hours = (12-t1[0])+t2[0]
+        if t2[1]<t1[1]:
+            hours = hours - 1
+            if t2[1] + t1[1] > 60:
+                min = t2[1] + t1[1] - 60
+                hours = hours + min * 60
+            if t2[1] + t1[1] == 60:
+                hours = hours + 1
+            else:
+                min = t2[1] + t1[1]
+                hours = hours + min * 60
 
-    def convert(time):
-        timelist = []
-        timelist = time.split(':')
-        return timelist
+    def convert(time, time2):
+        dt1 = []
+        dt1 = time.split(':')
+        dt2 = []
+        dt2 = time2.split(':')
+        dt1[0] = int(dt1[0])
+        dt1[1] = int(dt1[1])
+        dt2[0] = int(dt1[0])
+        dt1[1] = int(dt1[1])
+        
 
-print(Datetime.convert("04:01"))
+Req.calctd([12,00],[23,00])
