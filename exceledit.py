c12f4c71d24bbfbbc5a9e2963b0dfd58c5bed47e
jttw0010100 <0010100@student.isf.edu.hk>
2022-07-19 11:37:27 +0800

    modified main

--- exceledit.py
+++ exceledit.py
@@ -6,27 +6,18 @@ from openpyxl import load_workbook
 class EditExcel():
     writer = pd.ExcelWriter("test.xlsx", engine='xlsxwriter')
 
-    df2 = pd.DataFrame({
-                'Total Days': [1]},
-                )
+    def insert(dataframe,tab_name):
+        dataframe.to_excel(EditExcel.writer, sheet_name=tab_name)
 
-    df3 = pd.DataFrame({'Start Date': [0, 0, 0], 
-                'End Date': [0, 0, 0]},
-                index=['Year', 'Month', 'Day'] 
-                )
-
-    def insert(df,tab_name):
-        df.to_excel(EditExcel.writer, sheet_name=tab_name)
-
-    def specinsert(df,tab_name,tf,sc,sr):
-        df.to_excel(EditExcel.writer, sheet_name=tab_name, index = tf, startcol = sc, startrow = sr)
+    def specinsert(dataframe,tab_name,tf,sc,sr):
+        dataframe.to_excel(EditExcel.writer, sheet_name=tab_name, index = tf, startcol = sc, startrow = sr)
         
-    def replace(df, col, val):
-        df[col] = df[col].replace([0],[val])
+    def replace(dataframe, col, old, val):
+        dataframe[col] = dataframe[col].replace([old],[val])
 
     def test():
+        EditExcel.replace(EditExcel.df2, 'Total Days', 0, 100)
         EditExcel.insert(EditExcel.df3,'tab')
         EditExcel.specinsert(EditExcel.df2,'tab', False, 1, 5)
         EditExcel.writer.save()
 
-EditExcel.test()
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import exceledit 
 import pandas as pd
+
+class Temp():
+    ddf1 = pd.DataFrame({
+                'Total Days': [0]},
+                )
+
+    ddf2 = pd.DataFrame({'Start Date': [0, 0, 0], 
+                'End Date': [0, 0, 0]},
+                index=['Year', 'Month', 'Day'] 
+                )
+    def test():
+        exceledit.EditExcel.replace(Temp.ddf1, 'Total Days', 0, 100)
+        exceledit.EditExcel.insert(Temp.ddf2,'tab')
+        exceledit.EditExcel.specinsert(Temp.ddf1,'tab', False, 1, 5)
+        exceledit.EditExcel.writer.save()
+
+Temp.test()
